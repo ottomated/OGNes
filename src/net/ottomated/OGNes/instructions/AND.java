96b91d0bf14f407777f83929e92920f94a4e87f7
Ottomated <otto@ottomated.net>
2019-04-30 11:53:19 -0700

    Add AND class

---
+++ src/net/ottomated/OGNes/instructions/AND.java
@@ -0,0 +1,30 @@
+package net.ottomated.OGNes.instructions;
+
+import net.ottomated.OGNes.Cpu;
+
+class AND extends ReadInstruction {
+
+    void finalStep() {
+        int res = cpu.a & m;
+
+        cpu.setNegative(((res >> 7) & 1) == 1); // If the 7th bit is 1
+        cpu.setZero(res == 0);
+        cpu.a = res & 255; // Don't overflow
+    }
+
+    AND(Cpu cpu, AddressingMode mode) {
+        super(mode);
+        this.cpu = cpu;
+        done = false;
+        this.mode = mode;
+    }
+
+    @Override
+    public void cycle() {
+        steps[c].go();
+        c++;
+        if (length == c)
+            done = true;
+
+    }
+}
