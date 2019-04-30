e58c138d10e61e5d712f87e26be72535dc284a01
Ottomated <otto@ottomated.net>
2019-04-30 10:25:14 -0700

    Finish ADC

--- src/net/ottomated/OGNes/instructions/ADC.java
+++ src/net/ottomated/OGNes/instructions/ADC.java
@@ -6,6 +6,20 @@ class ADC extends Instruction {
 
     private int m; // Value to add to A + C
     private int loc; // Memory location of M
+    private int high; // High byte of addressing
+    private int pointer; // Used for indexed indirect, pointer in zero-page to memory location
+
+
+    private void finalStep() {
+        int res = cpu.a + m + (cpu.getCarry() ? 1 : 0);
+
+        cpu.setOverflow(((cpu.a ^ m) & 0x80) == 0 && // If the sign bit isn't set but should be
+                ((cpu.a ^ res & 0x80) != 0));
+        cpu.setCarry(res > 255);
+        cpu.setNegative(((res >> 7) & 1) == 1); // If the 7th bit is 1
+        cpu.setZero(res == 0);
+        cpu.a = res & 255; // Don't overflow
+    }
 
     ADC(Cpu cpu, Cpu.AddressingMode mode) {
         this.cpu = cpu;
@@ -17,14 +31,7 @@ class ADC extends Instruction {
                 steps = new Step[]{
                         () -> {
                             m = cpu.pop();
-                            int res = cpu.a + m + (cpu.carry ? 1 : 0);
-
-                            cpu.overflow = ((cpu.a ^ m) & 0x80) == 0 && // If the sign bit isn't set but should be
-                                    ((cpu.a ^ res & 0x80) != 0);
-                            cpu.carry = res > 255;
-                            cpu.negative = ((res >> 7) & 1) == 1; // If the 7th bit is 1
-                            cpu.zero = res == 0;
-                            cpu.a = res & 255; // Don't overflow
+                            finalStep();
                         },
                 };
                 break;
@@ -32,29 +39,117 @@ class ADC extends Instruction {
                 length = 2;
                 steps = new Step[]{
                         () -> loc = cpu.pop(),
-                        () -> cpu.a = cpu.a + cpu.peek(loc) + (cpu.carry ? 1 : 0),
+                        () -> {
+                            m = cpu.peek(loc);
+                            finalStep();
+                        },
+                };
+                break;
+            case INDEXED_ZERO_PAGE_X:
+                length = 3;
+                steps = new Step[]{
+                        () -> loc = cpu.pop(),
+                        () -> {
+                            loc += cpu.x;
+                            loc %= 0x100;
+                        },
+                        () -> {
+                            m = cpu.peek(loc);
+                            finalStep();
+                        },
+                };
+                break;
+            case ABSOLUTE:
+                length = 3;
+                steps = new Step[]{
+                        () -> loc = cpu.pop(),
+                        () -> high = cpu.pop(),
+                        () -> {
+                            m = cpu.peek(loc + high * 256);
+                            finalStep();
+                        }
                 };
                 break;
             case INDEXED_ABSOLUTE_X:
-                length = 4;
+                length = 3;
                 steps = new Step[]{
                         () -> loc = cpu.pop(),
-                        () -> loc += cpu.pop() * 256,
-                        () -> loc += cpu.x,
-                        () -> cpu.a = cpu.a + m + (cpu.carry ? 1 : 0),
+                        () -> {
+                            high = cpu.pop();
+                            loc += cpu.x;
+                        },
+                        () -> {
+                            if (loc > 255) { // Emulate fixing address
+                                c--;
+                                loc -= 256;
+                                high++;
+                                return;
+                            }
+                            m = cpu.peek(loc + high * 256);
+                            finalStep();
+                        }
                 };
                 break;
-
             case INDEXED_ABSOLUTE_Y:
-            case INDEXED_ZERO_PAGE_X:
-            case ABSOLUTE:
-                length = 4;
-                break;
-            case INDIRECT_INDEXED:
-                length = 5;
+                length = 3;
+                steps = new Step[]{
+                        () -> loc = cpu.pop(),
+                        () -> {
+                            high = cpu.pop();
+                            loc += cpu.y;
+                        },
+                        () -> {
+                            if (loc > 255) { // Emulate fixing address
+                                c--;
+                                loc -= 256;
+                                high++;
+                                return;
+                            }
+                            m = cpu.peek(loc + high * 256);
+                            finalStep();
+                        }
+                };
                 break;
             case INDEXED_INDIRECT:
-                length = 6;
+                length = 5;
+                steps = new Step[]{
+                        () -> pointer = cpu.pop(),
+                        () -> {
+                            pointer += cpu.x;
+                            pointer %= 256;
+                        },
+                        () -> {
+                            loc = cpu.peek(pointer);
+                        },
+                        () -> {
+                            high = cpu.peek(pointer + 1);
+                        },
+                        () -> {
+                            m = cpu.peek(loc + high * 256);
+                            finalStep();
+                        }
+                };
+                break;
+            case INDIRECT_INDEXED:
+                length = 4;
+                steps = new Step[]{
+                        () -> pointer = cpu.pop(),
+                        () -> loc = cpu.peek(pointer),
+                        () -> {
+                            high = cpu.peek(pointer + 1);
+                            loc += cpu.y;
+                        },
+                        () -> {
+                            if (loc > 255) { // Emulate fixing address
+                                c--;
+                                loc -= 256;
+                                high++;
+                                return;
+                            }
+                            m = cpu.peek(loc + high * 256);
+                            finalStep();
+                        }
+                };
                 break;
         }
 
@@ -63,11 +158,10 @@ class ADC extends Instruction {
 
     @Override
     public void cycle() {
-        if (length == c) {
-            done = true;
-            return;
-        }
         steps[c].go();
         c++;
+        if (length == c)
+            done = true;
+
     }
 }
